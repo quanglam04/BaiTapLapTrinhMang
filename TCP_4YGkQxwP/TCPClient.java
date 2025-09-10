91016d3a6383a386758e5a3858c47c1b32f8d81e
quanglam04 <trinhquanglam2k4@gmail.com>
2025-09-10 08:58:23 +0700

    update

--- TCP_4YGkQxwP/TCPClient.java
+++ TCP_4YGkQxwP/TCPClient.java
@@ -3,20 +3,21 @@ package BaiTap.TCP_4YGkQxwP;
 import BaiTap.Config.Config;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
- * @author Trịnh Quang Lâm
- * @since 09/09/2025<br>
- * [qCode]: 4YGkQxwP<br>
+ [Mã câu hỏi (qCode): 4YGkQxwP].  Một chương trình server cho phép kết nối qua giao thức TCP tại cổng 2208 (hỗ trợ thời gian giao tiếp tối đa cho mỗi yêu cầu là 5s). Yêu cầu là xây dựng một chương trình client tương tác với server sử dụng các luồng byte (BufferedWriter/BufferedReader) theo kịch bản sau: <br>
+ a.	Gửi một chuỗi gồm mã sinh viên và mã câu hỏi với định dạng "studentCode;qCode". Ví dụ: "B15DCCN999;EC4F899B"<br>
+ b.	Nhận một chuỗi ngẫu nhiên là danh sách các một số tên miền từ server<br>
+ Ví dụ: giHgWHwkLf0Rd0.io, I7jpjuRw13D.io, wXf6GP3KP.vn, MdpIzhxDVtTFTF.edu, TUHuMfn25chmw.vn, HHjE9.com, 4hJld2m2yiweto.vn, y2L4SQwH.vn, s2aUrZGdzS.com, 4hXfJe9giAA.edu<br>
+ c.	Tìm kiếm các tên miền .edu và gửi lên server<br>
+ Ví dụ: MdpIzhxDVtTFTF.edu, 4hXfJe9giAA.edu<br>
+ d.	Đóng kết nối và kết thúc chương trình.<br>
  */
 
 
--- TCP_rdFmVLrx/Client.java
+++ TCP_rdFmVLrx/Client.java
@@ -8,9 +8,14 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 /**
- * @author Trịnh Quang Lâm
- * @since 09/09/2025<br>
- * [qCode]: rdFmVLrx<br>
+ [Mã câu hỏi (qCode): rdFmVLrx].  Một chương trình server hỗ trợ kết nối qua giao thức TCP tại cổng 2206 (hỗ trợ thời gian giao tiếp tối đa cho mỗi yêu cầu là 5s). Yêu cầu xây dựng chương trình client thực hiện kết nối tới server sử dụng luồng byte dữ liệu (InputStream/OutputStream) để trao đổi thông tin theo thứ tự: <br>
+ a. Gửi mã sinh viên và mã câu hỏi theo định dạng "studentCode;qCode".<br>
+ Ví dụ: "B16DCCN999;C89DAB45"<br>
+ b. Nhận dữ liệu từ server là một chuỗi các số nguyên được phân tách bởi ký tự ",".<br>
+ Ví dụ: "8,4,2,10,5,6,1,3"<br>
+ c. Tính tổng của tất cả các số nguyên tố trong chuỗi và gửi kết quả lên server.<br>
+ Ví dụ: Với dãy "8,4,2,10,5,6,1,3", các số nguyên tố là 2, 5, 3, tổng là 10. Gửi lên server chuỗi "10".<br>
+ d. Đóng kết nối và kết thúc chương trình.<br>
  */
 
 
