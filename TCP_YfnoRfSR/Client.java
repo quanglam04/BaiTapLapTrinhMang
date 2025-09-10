63fe581dfad02450fa3d40a9a994caa6b71c7cae
quanglam04 <trinhquanglam2k4@gmail.com>
2025-09-10 08:48:04 +0700

    YfnoRfSR

--- TCP_YfnoRfSR/Client.java
+++ TCP_YfnoRfSR/Client.java
@@ -8,9 +8,11 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 /**
- * @author Trịnh Quang Lâm
- * @since 09/09/2025<br>
- * [qCode]: YfnoRfSR<br>
+ [Mã câu hỏi (qCode): YfnoRfSR].  Một chương trình máy chủ cho phép kết nối qua TCP tại cổng 2207 (hỗ trợ thời gian liên lạc tối đa cho mỗi yêu cầu là 5s), yêu cầu xây dựng chương trình (tạm gọi là client) thực hiện kết nối tới server tại cổng 2207, sử dụng luồng byte dữ liệu (DataInputStream/DataOutputStream) để trao đổi thông tin theo thứ tự: <br>
+ a.	Gửi chuỗi là mã sinh viên và mã câu hỏi theo định dạng "studentCode;qCode". Ví dụ: "B15DCCN999;1D25ED92" <br>
+ b.	Nhận lần lượt hai số nguyên a và b từ server <br>
+ c.	Thực hiện tính toán tổng, tích và gửi lần lượt từng giá trị theo đúng thứ tự trên lên server <br>
+ d.	Đóng kết nối và kết thúc <br>
  */
 
 
